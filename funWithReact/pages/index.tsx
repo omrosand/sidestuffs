f3787d8e5f5ca45fbcca10202089d69fbc5697ea
Ole M. Røsand <77734811+omrosand@users.noreply.github.com>
2022-09-12 13:56:18 +0200

    new test project

---
+++ funWithReact/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <h1>Dette er forsiden</h1>
+      <p>Dette er en paragraf</p>
+    </>
+  );
+};
+
+export default Home;

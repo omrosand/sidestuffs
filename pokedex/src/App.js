5ca1ba045691e78328a5bc20bf038adda91f2a28
Ole M. Røsand <77734811+omrosand@users.noreply.github.com>
2022-09-28 20:59:35 +0200

    aa

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [pokemons, setPokemons] = useState([]);
 
   const getPokemons = async () => {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=10");
     const data = await response.json();
     setPokemons(data?.results);
   };

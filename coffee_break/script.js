d6d83a4045ede9c3a375197b2ef5377c7b25c770
Ole M. Røsand <77734811+omrosand@users.noreply.github.com>
2022-09-28 13:39:59 +0200

    aa

--- coffee_break/script.js
+++ coffee_break/script.js
@@ -1,63 +1,69 @@
 // 2. This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
+var tag = document.createElement("script");
 
 tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
+var firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
 var player;
-  function onYouTubeIframeAPIReady() {
+function onYouTubeIframeAPIReady() {
+  var ctrlq = document.getElementById("youtube-audio");
+  ctrlq.innerHTML =
+    '<div id="myBtn" id="play-video" class=""/><div id="youtube-player"></div>';
+  ctrlq.onclick = toggleAudio;
 
-    var ctrlq = document.getElementById("youtube-audio");
-    ctrlq.innerHTML = '<div id="myBtn" id="play-video" class=""/><div id="youtube-player"></div>';
-    ctrlq.onclick = toggleAudio;
+  player = new YT.Player("youtube-player", {
+    height: "0",
+    width: "0",
+    videoId: ctrlq.dataset.video,
+    playerVars: {
+      autoplay: ctrlq.dataset.autoplay,
+      loop: ctrlq.dataset.loop,
+    },
+    events: {
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange,
+    },
+  });
 
-    player = new YT.Player('youtube-player', {
-      height: '0',
-      width: '0',
-      videoId: ctrlq.dataset.video,
-      playerVars: {
-        autoplay: ctrlq.dataset.autoplay,
-        loop: ctrlq.dataset.loop,
-      },
-      events: {
-        'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange 
-      } 
-    });
+  let listeningTo = document.getElementById("currentSong");
 
-let listeningTo = document.getElementById("currentSong");
+  function togglePlayButton(play) {
+    document.getElementById("myBtn").className = play
+      ? "fa fa-pause-circle fa-5x"
+      : "fa fa-play-circle fa-5x";
+    listeningTo.innerHTML = play
+      ? "<span class='animateCurrentSong'>Lofi hip hop <i style='font-size:13px' class='fas'>&#xf001;</i></span>" +
+        "<br>" +
+        "<a class='source' href='https://www.youtube.com/watch?v=jfKfPfyJRdk' target='_blank'>source</a>"
+      : "<i style='font-size:15px' class='fas'>&#xf6a9;</i>";
+  }
 
-function togglePlayButton(play) {    
-    document.getElementById("myBtn").className = play ? "fa fa-pause-circle fa-5x" : "fa fa-play-circle fa-5x";
-    listeningTo.innerHTML = play ? "<span class='animateCurrentSong'>Rainy Lofi Jazz Radio <i style='font-size:13px' class='fas'>&#xf001;</i></span>" + "<br>" + "<a class='source' href='https://www.youtube.com/watch?v=uxR_sTZnBtg' target='_blank'>source</a>" : "<i style='font-size:15px' class='fas'>&#xf6a9;</i>";
-}
-
-function toggleAudio() {
-    if ( player.getPlayerState() == 1 || player.getPlayerState() == 3 ) {
-      player.pauseVideo(); 
+  function toggleAudio() {
+    if (player.getPlayerState() == 1 || player.getPlayerState() == 3) {
+      player.pauseVideo();
       togglePlayButton(false);
-    } 
-    else {
-      player.playVideo(); 
+    } else {
+      player.playVideo();
       togglePlayButton(true);
-    } 
-}
+    }
+  }
 
-function onPlayerReady(event) {
+  function onPlayerReady(event) {
     player.setPlaybackQuality("small");
     //event.target.setVolume(50);
     document.getElementById("youtube-audio").style.display = "block";
     togglePlayButton(player.getPlayerState() !== 5);
-}
+  }
 
-function onPlayerStateChange(event) {
+  function onPlayerStateChange(event) {
     if (event.data === 0) {
-      togglePlayButton(false); 
+      togglePlayButton(false);
     }
-}};
+  }
+}
 
 document.getElementById("volumeDown").addEventListener("click", volumeDown);
 document.getElementById("volumeUp").addEventListener("click", volumeUp);
@@ -65,15 +71,13 @@ document.getElementById("volumeUp").addEventListener("click", volumeUp);
 let currentVolume = 50;
 
 function volumeDown() {
-  player.setVolume(currentVolume += -10);
+  player.setVolume((currentVolume += -10));
   console.log(currentVolume);
-  
 }
 function volumeUp() {
-  player.setVolume(currentVolume += 10);
+  player.setVolume((currentVolume += 10));
   console.log(currentVolume);
 }
-
 
 /*
 document.getElementById("myBtn").addEventListener("click", function (e) {

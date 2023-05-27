0b69ad44467749ee31cb855adae1ba449c9aa675
Ole M. Røsand <77734811+omrosand@users.noreply.github.com>
2023-05-27 16:09:05 +0200

    made by tag

--- coffee_break_v2/src/App.jsx
+++ coffee_break_v2/src/App.jsx
@@ -5,14 +5,29 @@ import Weather from "./components/Weather";
 
 function App() {
   return (
-    <main>
-      <header>
-        <h1>Kaffepause</h1>
-        <img src={logo} alt="off" />
-      </header>
-      <Player />
-      <Weather />
-    </main>
+    <>
+      <main>
+        <header>
+          <h1>Kaffepause</h1>
+          <img src={logo} alt="off" />
+        </header>
+        <Player />
+        <Weather />
+        <div className="madeBy">
+          <p>Laget av Ole M. Røsand</p>
+          <p>
+            <a
+              href="https://github.com/omrosand"
+              target="_blank"
+              rel="noreferrer"
+            >
+              github
+            </a>{" "}
+            | <a href="mailto: rosandolemartin@gmail.com">email</a>
+          </p>
+        </div>
+      </main>
+    </>
   );
 }
 
